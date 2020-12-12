389ec8aa358da38512b34782057a76c9bd458550
Tran Son Hoang <hayden.tran.d@gmail.com>
2020-12-12 15:31:03 +0800

    add setting for Deno on visual studio + tests folder

---
+++ tests/simple.test.ts
@@ -0,0 +1,13 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import starter from "../mod.ts";
+
+Deno.test("test starter function", (): void => {
+  assertEquals(starter("abc"), "abc");
+});
+
+Deno.test({
+  name: "test starter function",
+  fn(): void {
+    assertEquals(starter("foo bar"), "foo bar");
+  },
+});
